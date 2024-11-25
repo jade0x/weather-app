b4694e01a58c8b08f3346f91aee39c411f3ef8b5
winx33 <119149884+winx33@users.noreply.github.com>
2024-11-25 15:21:00 -0600

    Added header, slowed placeholder animation

--- src/index.js
+++ src/index.js
@@ -157,7 +157,7 @@ function printLetter(string, el) {
     if (phCount < arr.length) {
       printLetter(origString, el);
     }
-  }, randDelay(50, 90));
+  }, randDelay(400, 600));
 }
 
 function placeholder() {
